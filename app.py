72bd25dda14003091bdc1941f5bf9ce7e44f8a88
muzmmil_pathan <muzmmil_pathan@apple.com>
2023-03-12 15:39:20 +0530

    print host_name

--- app.py
+++ app.py
@@ -3,26 +3,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
-config.load_incluster_config()  # Load the Kubernetes configuration from the cluster
+config.load_kube_config()  # Load the Kubernetes configuration from the kubeconfig file
 
 api_instance = client.CoreV1Api()
 
-pod_name = None
+host_name = None
 try:
     pod_name = open("/var/run/secrets/kubernetes.io/serviceaccount/pod.name", "r").read()
+    pod = api_instance.read_namespaced_pod(name=pod_name, namespace='default')
+    command = ['/bin/bash', '-c', 'hostname']  # Bash command to get the hostname of the pod's node
+    resp = api_instance.exec_namespaced_pod(name=pod_name, namespace='default', container=pod.spec.containers[0].name, command=command)
+    host_name = resp.output.decode().strip()  # Get the output of the command and strip newline characters
 except Exception as e:
     # Handle error if running outside of Kubernetes cluster
     pass
 
-if pod_name:
-    pod = api_instance.read_namespaced_pod(name=pod_name, namespace='default')
-    container_name = pod.spec.containers[0].name
-else:
-    container_name = None
-
 @app.route('/')
 def hello_world():
-    return 'Hello from container {}'.format(container_name)
+    return 'Hello from host {}'.format(host_name)
 
 if __name__ == "__main__":
      app.run(host='0.0.0.0', port=5000)
