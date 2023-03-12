1786f17a4d6b9af9106f78721fd356327ff18bfb
muzmmil_pathan <muzmmil_pathan@apple.com>
2023-03-12 14:46:25 +0530

    added  Kubernetes API to retrieve the container name

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
+from kubernetes import client, config
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+config.load_incluster_config()  # Load the Kubernetes configuration from the cluster
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+api_instance = client.CoreV1Api()
+
+pod_name = config.pod_name  # Get the name of the current pod
+
+pod = api_instance.read_namespaced_pod(name=pod_name, namespace='default')
+
+container_name = None
+
+for container in pod.spec.containers:
+    if container.env is not None:
+        for env_var in container.env:
+            if env_var.name == 'MY_CONTAINER_NAME_ENV_VARIABLE':
+                container_name = env_var.value
+
+@app.route('/')
+def hello_world():
+    return 'Hello from container {}'.format(container_name)
 
