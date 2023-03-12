4f76057c04635e34e395d0c7e696278ff2b1ea10
muzmmil_pathan <muzmmil_pathan@apple.com>
2023-03-12 15:48:50 +0530

    bug fixed for host_name

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
-from kubernetes import client, config
 from flask import Flask
 
 app = Flask(__name__)
 
-config.load_kube_config()  # Load the Kubernetes configuration from the kubeconfig file
-
-api_instance = client.CoreV1Api()
-
 host_name = None
 try:
-    pod_name = open("/var/run/secrets/kubernetes.io/serviceaccount/pod.name", "r").read()
-    pod = api_instance.read_namespaced_pod(name=pod_name, namespace='default')
     command = ['/bin/bash', '-c', 'hostname']  # Bash command to get the hostname of the pod's node
-    resp = api_instance.exec_namespaced_pod(name=pod_name, namespace='default', container=pod.spec.containers[0].name, command=command)
-    host_name = resp.output.decode().strip()  # Get the output of the command and strip newline characters
+    host_name = command  # Get the output of the command and strip newline characters
 except Exception as e:
     # Handle error if running outside of Kubernetes cluster
     pass
